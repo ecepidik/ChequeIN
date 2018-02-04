725402f42305288e992411a8f7050d1202c3c9da
Maxence Frenette <maxence.frenette@gmail.com>
2018-02-04 16:28:46 -0500

    Added the navigation menu

---
+++ frontend/src/app/create-cheque-req/create-cheque-req.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+/**
+ * This components contains the Cheque Req creation form.
+ */
+@Component({
+  selector: 'app-create-cheque-req',
+  templateUrl: './create-cheque-req.component.html',
+  styleUrls: ['./create-cheque-req.component.scss']
+})
+export class CreateChequeReqComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
+}
