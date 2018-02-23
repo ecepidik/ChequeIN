7492d73bae0bf4ebf17192b1caaa2d526a87ee30
Eva Suska <eva.suska@mail.mcgill.ca>
2018-02-22 20:27:05 -0500

    added a cheque req class for cheque reqs already submitted and the testing service call for it

--- frontend/src/app/api/api.service.ts
+++ frontend/src/app/api/api.service.ts
@@ -1,20 +1,22 @@
-import { Injectable } from '@angular/core';
-import { AuthHttp } from 'angular2-jwt';
-import { Observable } from 'rxjs/Observable';
-import { environment } from '../../environments/environment';
-import { ChequeReq } from './cheque-req';
-import { User } from './user';
-import { Account } from './account';
+import {Injectable} from '@angular/core';
+import {AuthHttp} from 'angular2-jwt';
+import {Observable} from 'rxjs/Observable';
+import {environment} from '../../environments/environment';
+import {ChequeReq} from './cheque-req';
+import {User} from './user';
+import {Account} from './account';
+import {ChequeReq2} from './cheque-req2';
 
 @Injectable()
 export class ApiService {
-  constructor(private authHttp: AuthHttp) {}
+  constructor(private authHttp: AuthHttp) {
+  }
 
   /**
    * Gets information about the currently logged-in user.
    */
   getUser(): Observable<User> {
-    return Observable.of({ name: 'Jonh Doe' }); // TODO: actually call the API
+    return Observable.of({name: 'Jonh Doe'}); // TODO: actually call the API
   }
 
   /**
@@ -35,4 +37,40 @@ export class ApiService {
   submitChequeReq(chequeReq: ChequeReq): Observable<void> {
     return Observable.of(); // TODO: Make an actual API call
   }
-}
+
+  /** 
+   * Gets information about the submitted cheque reqs 
+   */
+   getChequeReqs(): Observable<Array<ChequeReq2>> {   //TODO: get multiple cheq reqs 
+    return Observable.of(
+      [
+        {
+          preTax: 15,
+          GST: 1,
+          PST: 1.33,
+          HST: 0.66,
+          description: 'ergrtgrt',
+          onlinePurchase: '',
+          approver: 'Jane Doe',
+          account: Account | undefined,
+          freeFood: false,
+          mailCheque: false,
+          mailingAddress: '123 Mountain Ave',
+        },
+        {
+          preTax: 0,
+          GST: 0,
+          PST: 0,
+          HST: 0,
+          description: 'ergrtgrt',
+          onlinePurchase: '',
+          approver: 'Jane Doe',
+          account: Account | undefined,
+          freeFood: false,
+          mailCheque: false,
+          mailingAddress: '123 Mountain Ave',
+        }
+      ]); // TODO: actually call the API 
+    }
+
+  }
