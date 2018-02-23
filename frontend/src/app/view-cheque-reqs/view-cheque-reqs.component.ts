d9f107ca44f0b4c1ccfd452f0b58938724a9c566
Eva Suska <eva.suska@mail.mcgill.ca>
2018-02-22 20:28:45 -0500

    started making table for view cheque req page

--- frontend/src/app/view-cheque-reqs/view-cheque-reqs.component.ts
+++ frontend/src/app/view-cheque-reqs/view-cheque-reqs.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api/api.service';
+import { Observable } from 'rxjs/Observable';
+import { Account } from '../api/account';
+import { ChequeReq2 } from '../api/cheque-req2';
 
 @Component({
   selector: 'app-view-cheque-reqs',
@@ -6,10 +10,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-cheque-reqs.component.scss']
 })
 export class ViewChequeReqsComponent implements OnInit {
+  chequeReqs$: Observable<[ChequeReq2]>;
 
   constructor() { }
 
   ngOnInit() {
+    this.chequeReqs$ = this.api.getChequeReqs();
   }
 
 }
