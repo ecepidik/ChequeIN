204f291dce0e9124c4c752279fe1e7babdc87a02
Maxence Frenette <maxence.frenette@gmail.com>
2018-02-24 00:44:07 -0500

    Oops. I forgot to add the actual component in the previous commit.

---
+++ frontend/src/app/view-cheque-req/view-cheque-req.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+/**
+ * Displays details about a single cheque req. Can be accessed with the url /cheque-req/:id
+ */
+@Component({
+  selector: 'app-view-cheque-req',
+  templateUrl: './view-cheque-req.component.html',
+  styleUrls: ['./view-cheque-req.component.scss']
+})
+export class ViewChequeReqComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
