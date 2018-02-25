ce85892d3e8ddf6bbb1e61b563fb66dd65f14cd4
Maxime Plante <maxime.pl@outlook.com>
2018-02-25 10:58:02 -0500

    Removed disableauthentication options from default option file
    Prevent people from changing the value to true by mistake.

--- backend/ChequeIN/Configurations/Authentication.cs
+++ backend/ChequeIN/Configurations/Authentication.cs
@@ -3,6 +3,12 @@ namespace ChequeIN.Configurations
 {
     public class Authentication
     {
+        public Authentication()
+        {
+            DisableAuthentication = false;
+            DevelopmentUserId = "";
+        }
+
         public bool DisableAuthentication { get; set; }
         public string DevelopmentUserId { get; set; }
     }
