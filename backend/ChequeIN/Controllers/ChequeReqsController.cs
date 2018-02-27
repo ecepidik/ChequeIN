6e10c1ee55a693b4ed8002d03c96f4f1a6f15cae
matlapo <mathieu.lapointe3@mail.mcgill.ca>
2018-02-27 17:24:16 -0500

    Commented code to return mock objects to un-block the frontend

--- backend/ChequeIN/Controllers/ChequeReqsController.cs
+++ backend/ChequeIN/Controllers/ChequeReqsController.cs
@@ -5,30 +5,38 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ChequeIN.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Options;
 
 namespace ChequeIN.Controllers
 {
     [Route("api/[controller]")]
     public class ChequeReqsController : Controller
     {
+        private Configurations.Authentication _authSettings;
+
+        public ChequeReqsController(IOptions<Configurations.Authentication> authSettings)
+        {
+            _authSettings = authSettings.Value;
+        }
+
         // GET api/ChequeReqs
         [HttpGet]
         public IActionResult Get()
         {
+            // bool exists = Database.ChequeReqs.TryGetAllChequeReqs("3033", out List<ChequeReq> cheques);
+            // if (!exists) {
+            //     return Ok(Enumerable.Empty<ChequeReq>().ToList<ChequeReq>());
+            // }
+            // else {
+            //     return Ok(cheques);
+            // }
             return Ok(Database.ChequeReqs.GetAllChequeReqs());
         }
 
         [HttpPost]
         public IActionResult Create([FromBody] ChequeReq cheque)
         {
-            // if (cheque == null)
-            //     return BadRequest();
-
-            Console.WriteLine(cheque);
-
-            var t = ModelState.IsValid;
-
-            return Ok(t);
+            return Ok(true);
         }
     }
 }
--- backend/ChequeIN/Database/ChequeReqs.cs
+++ backend/ChequeIN/Database/ChequeReqs.cs
@@ -16,5 +16,23 @@ namespace ChequeIN.Database
                 return (IEnumerable<ChequeReq>)context.ChequeReqs.ToList();
             }
         }
+
+        public static bool TryGetAllChequeReqs(string id, out List<ChequeReq> cheques)
+        {
+            using (var context = new DatabaseContext())
+            {
+                context.Database.EnsureCreated();
+
+                bool userExists = Users.TryGetUserById(id, out UserProfile user);
+
+                if (!userExists) {
+                    cheques = null;
+                    return false;
+                }
+
+                cheques = user.SubmittedChequeReqs.ToList<ChequeReq>();
+                return true;
+            }
+        }
     }
 }
