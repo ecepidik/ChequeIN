ca668355cda8adfae214dfbac6b78cb029710a17
matlapo <mathieu.lapointe3@mail.mcgill.ca>
2018-03-01 11:44:40 -0500

    Added constructor for authentication in users controller

--- backend/ChequeIN/Controllers/UsersController.cs
+++ backend/ChequeIN/Controllers/UsersController.cs
@@ -6,25 +6,30 @@ using Microsoft.AspNetCore.Mvc;
 using ChequeIN;
 using ChequeIN.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Options;
 
 namespace ChequeIN.Controllers
 {
     [Route("api/[controller]")]
     public class UsersController : Controller
     {
-        // GET api/users
+        private Configurations.Authentication _authSettings;
+
+        public UsersController(IOptions<Configurations.Authentication> authSettings) {
+            _authSettings = authSettings.Value;
+        }
+
+        // GET /api/users
         [HttpGet]
         [Authorize]
-        public IEnumerable<UserProfile> Get()
-        {
+        public IEnumerable<UserProfile> Get() {
             return Database.Users.GetAllUsers();
         }
 
         // GET api/users/id
         [HttpGet("{id}")]
         [Authorize]
-        public IActionResult Get(string id)
-        {
+        public IActionResult Get(string id) {
             var exists = Database.Users.TryGetUserById(id, out UserProfile user);
 
             if (!exists) {
