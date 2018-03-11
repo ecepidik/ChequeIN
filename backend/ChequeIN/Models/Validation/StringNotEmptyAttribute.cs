5e53987b3bdd08bdc11c7d81c1b41bab19ab76af
AlexisGJ <alexis.giguere-joannette@mail.mcgill.ca>
2018-03-11 19:48:16 -0400

    FO.H.10 - Approved By Field

---
+++ backend/ChequeIN/Models/Validation/StringNotEmptyAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+public class StringNotEmptyAttribute : ValidationAttribute
+{
+    public override bool IsValid(object value)
+    {
+        if (String.IsNullOrEmpty(value.ToString()))
+        {
+            return false;
+        }
+        return true;
+
+    }
+}
